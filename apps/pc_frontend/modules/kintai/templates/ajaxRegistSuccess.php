bb2bdf88b2a91136de81cf2e14aabc121f9c2c32
Shouta Kashiwagi <kashiwagi@tejimaya.com>
2011-10-20 11:16:42 +0900

    fix #3.
    fix #3.

--- apps/pc_frontend/modules/kintai/templates/ajaxRegistSuccess.php
+++ apps/pc_frontend/modules/kintai/templates/ajaxRegistSuccess.php
@@ -55,19 +55,18 @@ $(function(){
 <div id="kintai_explain_open"><a id="kintai_explain_open_link">▼勤怠入力の説明</a></div>
 <div id="kintai_explain" style="display: none;">
 <h4>勤怠入力の方法</h4>
-例１）会社に<span class="op_font" style="color:#00FF00;"><span class="op_font" style="font-size:large">出社</span></span>して、<span class="op_font" style="color:#3366FF;"><span class="op_font" style="font-size:large">10:00</span></span>に出勤、<span class="op_font" style="color:#FF0000;"><span class="op_font" style="font-size:large">19:00</span></span>に退勤した場合。<br />
+例１）会社に<span class="op_font" style="color:#00FF00;"><span class="op_font" style="font-size:large">出社</span></span>して、<span class="op_font" style="color:#3366FF;"><span class="op_font" style="font-size:large">10:00</span></span>に出勤、<span class="op_font" style="color:#FF0000;"><span class="op_font" style="font-size:large">25:00(翌日)</span></span>に退勤した場合。<br />
 <span class="op_font" style="color:#00FF00;">出勤</span>なので、
-<span class="op_font" style="font-size:large"><span class="op_font" style="color:#00FF00;">S</span><span class="op_font" style="color:#3366FF;">1000</span><span class="op_font" style="color:#FF0000;">1900</span></span>
+<span class="op_font" style="font-size:large"><span class="op_font" style="color:#00FF00;">S</span><span class="op_font" style="color:#3366FF;">1000</span><span class="op_font" style="color:#FF0000;">2500</span></span>
 と入力します。<br />
 <br />
-例２）<span class="op_font" style="color:#CC99FF;"><span class="op_font" style="font-size:large">在宅</span></span>で<span class="op_font" style="color:#3366FF;"><span class="op_font" style="font-size:large">12:00</span></span>から<span class="op_font" style="color:#FF0000;"><span class="op_font" style="font-size:large">16:00</span></span>まで働>いた場合<br /> 
+例２）<span class="op_font" style="color:#CC99FF;"><span class="op_font" style="font-size:large">在宅</span></span>で<span class="op_font" style="color:#3366FF;"><span class="op_font" style="font-size:large">09:00</span></span>から<span class="op_font" style="color:#FF0000;"><span class="op_font" style="font-size:large">16:00</span></span>まで働いた場合<br /> 
 <span class="op_font" style="color:#CC99FF;">在宅</span>なので、
-<span class="op_font" style="font-size:large"><span class="op_font" style="color:#CC99FF;">Z</span><span class="op_font" style="color:#3366FF;">1200</span><span class="op_font" style="color:#FF0000;">1600</span></span>
+<span class="op_font" style="font-size:large"><span class="op_font" style="color:#CC99FF;">Z</span><span class="op_font" style="color:#3366FF;">0900</span><span class="op_font" style="color:#FF0000;">1600</span></span>
 と入力します。<br />
-・休憩時間の項目は休憩した時間（分単位）で入力してください。<br />
-・コメントには、今日１日い行った業務を簡潔に入力してください。<br />
+・「<b>休憩時間</b>」は、休憩した時間（分単位）を入力してください。<br />
+・「<b>コメント</b>」は、今日１日行った業務を簡潔に入力してください。<br />
 <br />
 <a id="kintai_explain_close_link">▲説明を閉じる</a><br />
 </div>
-
 </div>
