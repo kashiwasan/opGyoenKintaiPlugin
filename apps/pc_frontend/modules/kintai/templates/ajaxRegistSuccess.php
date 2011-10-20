f6cd96badabb17112ae7b084c6338780d1d3f04d
Shouta Kashiwagi <kashiwagi@tejimaya.com>
2011-10-20 10:47:58 +0900

    fix #3.
    fix #3.

--- apps/pc_frontend/modules/kintai/templates/ajaxRegistSuccess.php
+++ apps/pc_frontend/modules/kintai/templates/ajaxRegistSuccess.php
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 $(function(){
   $("#kintai_regist_loading").hide();
-    $("#kintai_regist_submit").click(function(){
+  $("#kintai_regist_submit").click(function(){
       $("#kintai_regist_loading").show();
       $("#regist_msg").hide();
       Data = $("#kintai_regist_data").val();
@@ -24,6 +24,16 @@ $(function(){
     });
     return false;
   });
+  
+  $("#kintai_explain_open_link").click(function(){
+    $("#kintai_explain_open").fadeOut("1000");
+    $("#kintai_explain").fadeIn("1000");
+  });
+  $("#kintai_explain_close_link").click(function(){
+    $("#kintai_explain").fadeOut("1000");
+    $("#kintai_explain_open").fadeIn("1000");
+  });
+
 });
 </script>
 <div class="partsHeading"><h3><?php echo $nickname; ?>さんの今日の勤怠を登録する</h3></div>
@@ -40,4 +50,28 @@ $(function(){
 <tr>
 <td><label for="comment">作業内容コメント</label></td><td><textarea name="comment" id="kintai_regist_comment"><?php echo $comment; ?></textarea></td></tr>
 <tr><td></td><td><input type="submit" name="submit" id="kintai_regist_submit" value="確認する" /></td></tr></table>
-<a href="./kintai/ajaxRegistEasy?y=<?php echo $y; ?>&m=<?php echo $m; ?>&d=<?php echo $d; ?>" rel="internal">簡単入力モードにする</a></div>
+<a href="./kintai/ajaxRegistEasy?y=<?php echo $y; ?>&m=<?php echo $m; ?>&d=<?php echo $d; ?>" rel="internal">簡単入力モードにする</a><br />
+
+<div id="kintai_explain_open"><a id="kintai_explain_open_link">▼勤怠入力の説明</a></div>
+<div id="kintai_explain" style="display: none;">
+<h4>勤怠入力の方法</h4>
+例１）会社に<span class="op_font" style="color:#00FF00;"><span class="op_font" style="font-size:large">出社</span></span>して、<span class="op_font" style="color:#3366FF;"><span class="op_font" style="font-size:large">10:00</span></span>に出勤、<span class="op_font" style="color:#FF0000;"><span class="op_font" style="font-size:large">19:00</span></span>に退勤した場合。<br />
+<br />
+<span class="op_font" style="color:#00FF00;">出勤</span>なので、<br />
+<span class="op_font" style="font-size:large"><span class="op_font" style="color:#00FF00;">S</span><span class="op_font" style="color:#3366FF;">1000</span><span class="op_font" style="color:#FF0000;">1900</span></span><br />
+と入力します。<br />
+<br />
+例２）<span class="op_font" style="color:#CC99FF;"><span class="op_font" style="font-size:large">在宅</span></span>で<span class="op_font" style="color:#3366FF;"><span class="op_font" style="font-size:large">12:00</span></span>から<span class="op_font" style="color:#FF0000;"><span class="op_font" style="font-size:large">16:00</span></span>まで働>いた場合<br /> 
+<br />
+<span class="op_font" style="color:#CC99FF;">在宅</span>なので、<br />
+<span class="op_font" style="font-size:large"><span class="op_font" style="color:#CC99FF;">Z</span><span class="op_font" style="color:#3366FF;">1200</span><span class="op_font" style="color:#FF0000;">1600</span></span><br />
+と入力します。<br />
+<br />
+<br />
+・休憩時間の項目は休憩した時間（分単位）で入力してください。<br />
+・コメントには、今日１日い行った業務を簡潔に入力してください。<br />
+<br />
+<a id="kintai_explain_close_link">▲説明を閉じる</a><br />
+</div>
+
+</div>
