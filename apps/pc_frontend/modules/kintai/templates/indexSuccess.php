d682d677b54d8f6d033415bdd53530c64d238b09
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-12 17:44:08 +0900

    baseurl の指定方法を変更 (url_for('@homepage', array('absolute' => true));)

--- apps/pc_frontend/modules/kintai/templates/indexSuccess.php
+++ apps/pc_frontend/modules/kintai/templates/indexSuccess.php
@@ -13,9 +13,9 @@ var kintaiMemberId = "<?php echo $viewMember; ?>";
   <td>${comment}</td>
   <td>
   {{if flag=="1"}}
-  <a rel="prettyPopinEdit" href="<?php echo url_for('@homepage'); ?>/kintai/ajaxEdit?y=${y}&m=${m}&d=${d}">編集</a>
+  <a rel="prettyPopinEdit" href="<?php echo url_for('@homepage', array('absolute' => true)); ?>/kintai/ajaxEdit?y=${y}&m=${m}&d=${d}">編集</a>
   {{else}}
-  <a rel="prettyPopinRegist" href="<?php echo url_for('@homepage'); ?>/kintai/ajaxRegist?y=${y}&m=${m}&d=${d}">新規登録</a>
+  <a rel="prettyPopinRegist" href="<?php echo url_for('@homepage', array('absolute' => true)); ?>/kintai/ajaxRegist?y=${y}&m=${m}&d=${d}">新規登録</a>
   {{/if}}
   </td>
 </tr>
@@ -23,14 +23,14 @@ var kintaiMemberId = "<?php echo $viewMember; ?>";
 <tr><td>${date}</td><td>出社: ${kintai1} (${rest1}) <br /> 在宅: ${kintai2} (${rest2})</td><td>${comment}</td><td></td></tr>
 <?php endif; ?>
 </script>
-<script type="text/javascript" src="<?php echo url_for('@homepage'); ?>/opGyoenKintaiPlugin/js/kintailist.js"></script>
+<script type="text/javascript" src="<?php echo url_for('@homepage', array('absolute' => true)); ?>/opGyoenKintaiPlugin/js/kintailist.js"></script>
 <div class='partsHeading'><h3><?php echo $member_name; ?>さんの勤怠</h3></div>
 <div class='block'>
 <?php if (is_null($member_editablelink) && $currentMember==$viewMember) : ?>
 <div id="kintai-loader" style="width: 100px; float: center; text-align: center;">
-<img src="<?php echo url_for('@homepage'); ?>/opGyoenKintaiPlugin/css/images/loader-bouncing-white-gray.gif" alt="loader" />
+<img src="<?php echo url_for('@homepage', array('absolute' => true)); ?>/opGyoenKintaiPlugin/css/images/loader-bouncing-white-gray.gif" alt="loader" />
 </div>
-<table width='100%' id="kintai-list" data-baseurl="<?php echo url_for('@homepage'); ?>" style="display: none;">
+<table width='100%' id="kintai-list" data-baseurl="<?php echo url_for('@homepage', array('absolute' => true)); ?>" style="display: none;">
 <tr><td width='17%'>Y / m / d</td><td width='25%'>勤怠</td><td width='33%'>作業コメント</td><td width='25%'></td></tr>
 </table>
 <br />
